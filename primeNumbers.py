656d2ecc059cce5c16ae1b2b7248cf9e12cc0fef
DIDAH-ASEEY <aseeyd@gmail.com>
2017-01-17 11:20:27 +0300

    Three lines of code added to check if the number in the loop is a prime number

--- primeNumbers.py
+++ primeNumbers.py
@@ -3,4 +3,14 @@ def is_Prime(n):
     if type(n)==int:
 
         for number in range(o,n+1):
+            if number >= 0:
+                for i in range(2,number):
+                    if (number%i)==0:
+                        break
+                else:
+                    print number
+
+
+                       
+            
             
