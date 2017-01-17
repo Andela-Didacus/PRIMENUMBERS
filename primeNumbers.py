30790d1080848bace9654f6a21ac8a9f9534f91f
DIDAH-ASEEY <aseeyd@gmail.com>
2017-01-17 11:37:58 +0300

    added an if statement that checks errors in user input

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,18 +1,30 @@
 def is_Prime(n):
 
-    if type(n)==int and n>0:
+    if type(n)==int and n>0:                    #checks for type error and ensures n is not a positive number     
 
-        for number in range(o,n+1):
-            if number >= 0:
-                for i in range(2,number):
+        for number in range(0,n+1):
+            if number >= 1:
+                for i in range(2,number):       #checks if the number is only divisible by 1 and itself
                     if (number%i)==0:
                         break
                 else:
                     print number
+            
+    else:
+        return "TypeError"
+
+        
 
 n = input("Enter a range: ")
 
-is_Prime(n)
+if type(n)==int:                                #checks the input from the user 
+    
+    is_Prime(n)
+    
+else:
+    
+    print  "TypeError"
+
                        
             
             
